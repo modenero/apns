cdd0d1476a0f9625f1acf8647f01a7f9c26e51c3
Shomari <nyusternie@sdot.io>
2022-02-25 14:56:37 -0700

    Update for dedicated APNS server.

--- api/bin/apnsd.js
+++ api/bin/apnsd.js
@@ -3,19 +3,15 @@
 const { ethers } = require('ethers')
 const Moralis = require('moralis/node')
 const PouchDB = require('pouchdb')
+// const { v4: uuidv4 } = require('uuid')
 
 /* Set database authorization. */
-const dbAuth = process.env.DB_AUTH
+const apnsAuth = process.env.APNS_AUTH
+const polygonAuth = process.env.POLYGON_AUTH
 
-;(async () => {
-    // const db = new PouchDB('apns')
-    const db = new PouchDB(`http://apns:${dbAuth}@localhost:5984/apns`)
-
-    const val = await db.info()
-        .catch(err => console.error(err))
-    console.log('DB INFO', val)
-
-})()
+// const db = new PouchDB('apns')
+const apnsDb = new PouchDB(`http://apns:${apnsAuth}@localhost:5984/apns`)
+const polygonMainnetDb = new PouchDB(`http://polygon:${polygonAuth}@localhost:5984/polygon_mainnet`)
 
 /* Set account SID. */
 const accountSid = process.env.TWILIO_ACCOUNT_SID
@@ -27,13 +23,23 @@ const authToken = process.env.TWILIO_AUTH_TOKEN
 const client = require('twilio')(accountSid, authToken)
 
 /* Set server URL. */
-const serverUrl = 'https://yfuoyjjd4i0z.usemoralis.com:2053/server'
+const serverUrl = 'https://sdxoimq5qr9a.usemoralis.com:2053/server'
 
 /* Set application id. */
-const appId = '0dtJTXF3zmhSrBBLCJ0LB9UBEfm0X2DGKbwx6Z1I'
+const appId = 'pzls6wPNQQtaVnMjubFYnnIWjcghkYfRW1Ytgot1'
+
+/* Set master key. */
+const masterKey = 'oO4LJwOOewl8nY8sTL4CgDU7KVPFzGid7itc8LoF'
+
+/* Initialize Web3. */
+const web3 = new Moralis.Web3()
 
 /* Moralis init code */
-Moralis.start({ serverUrl, appId })
+Moralis.start({ serverUrl, appId, masterKey })
+
+// const PolygonAcct = '0xBCc532C9c2052f09DF7E8b65Cd2845f55de34ac6' // MetaMask
+const PolygonAcct = '0xE2266286745fEFdDeC42D895abC85a33710a2078' // Brave
+const AvalancheSponsor = '0xDC1C3Eb7AD2a1ABF0CBb1B115B67ddd4cfAe5B66'
 
 const sendMsg = () => {
     /* Set message. */
@@ -47,8 +53,25 @@ const sendMsg = () => {
 }
 
 const init = async () => {
-    let query = new Moralis.Query('Game')
-    let subscription = await query.subscribe()
+    // let query = new Moralis.Query('APNS')
+    // let subscription = await query.subscribe()
+
+    /* Initialize Polygon transactions query. */
+    const query = new Moralis.Query('MaticTransactions')
+
+    /* Set query address. */
+    query.equalTo('to_address', PolygonAcct)
+
+    /* Subscribe for real-time updates. */
+    const subscription = await query.subscribe()
+
+    /* Set subscriptions handler. */
+    subscription.on('create', function(data) {
+        console.log('CREATED', data)
+
+        const amountEth = web3.utils.fromWei(data.attributes.value)
+        console.log(`${amountEth} deposited to Polygon`)
+    })
 
     // console.log('SUBSCRIPTION', subscription);
 
@@ -101,8 +124,8 @@ const query = async () => {
  */
 const manager = async () => {
     /* Set node URL. */
-    // const NODE_URL = 'wss://speedy-nodes-nyc.moralis.io/39f5474b84a2f39277aea60a/avalanche/mainnet/ws'
-    const NODE_URL = 'wss://speedy-nodes-nyc.moralis.io/39f5474b84a2f39277aea60a/avalanche/testnet/ws'
+    const NODE_URL = 'wss://speedy-nodes-nyc.moralis.io/39f5474b84a2f39277aea60a/avalanche/mainnet/ws'
+    // const NODE_URL = 'wss://speedy-nodes-nyc.moralis.io/39f5474b84a2f39277aea60a/avalanche/testnet/ws'
 
     /* Set provider. */
     const provider = new ethers.providers.WebSocketProvider(NODE_URL)
@@ -139,16 +162,51 @@ const manager = async () => {
         const transaction = await provider.getTransaction(tx)
         // console.log('TRANSACTION', transaction)
 
-        if (transaction.from === address || transaction.to === address) {
-            console.log('\nFOUND YOUR TRANSACTION', transaction)
+        const pkg = {
+            _id: transaction.hash,
+            ...transaction,
         }
+
+        const results = await polygonMainnetDb.put(pkg)
+            .catch(err => console.error('LOGS ERROR:', err))
+        console.log('RESULTS', results)
+
+        // if (transaction.from === address || transaction.to === address) {
+        //     console.log('\nFOUND YOUR TRANSACTION', transaction)
+        // }
     })
 
 }
 
+const watcher = async () => {
+    // const type = 'watchAvaxAddress' // Avalanche
+    // const type = 'watchBscAddress' // Binance Smart Chain
+    // const type = 'watchEthAddress' // Ethereum
+    // const type = 'watchFtmAddress' // Fantom
+    const type = 'watchPolygonAddress' // Polygon
+
+    const result = await Moralis.Cloud
+        .run(type, {
+            address: PolygonAcct,
+            // address: AvalancheSponsor,
+        }, {
+            useMasterKey: true,
+        })
+        .catch(err => console.error(err))
+    // const result = await Moralis.Cloud
+    //     .run('watchEthAddress', {
+    //         address: PolygonAcct,
+    //         syncHistorical: false,
+    //         useMasterKey: true,
+    //     })
+    //     .catch(err => console.error(err))
+    console.log('RESULT (watchXxxAddress):', result)
+}
+
 // init()
 // query()
-manager()
+// manager()
+watcher()
 
 // subscription.unsubscribe()
 
