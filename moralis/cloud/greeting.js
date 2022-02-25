1b97ff658cc3145ee31a11b4f5173e6867a2b98b
Shomari <nyusternie@sdot.io>
2022-02-25 14:57:25 -0700

    Create Moralis Cloud Functions samples.

---
+++ moralis/cloud/greeting.js
@@ -0,0 +1,3 @@
+const greeting = () => {
+    return 'hi there!'
+}
---
+++ moralis/cloud/hello.js
@@ -0,0 +1,7 @@
+Moralis.Cloud.define('hello', async (request) => {
+    // access the DB
+    // access blockchain
+    // access 3rd-party APIs
+    // any Logic
+    return getGreeting()
+})
